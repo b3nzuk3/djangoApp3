49288eccb916ce0130080e089e1ac31891e660f1
b3nzuk3 <hussenito7@gmail.com>
2022-06-07 12:04:40 +0300

    added crispy forms for the styling

--- django_app1/settings.py
+++ django_app1/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = 'static/'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL= 'image-home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
