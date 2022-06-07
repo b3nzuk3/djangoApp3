3480f156a68c3bc0e5762bff99a8df20bc5a6d80
b3nzuk3 <hussenito7@gmail.com>
2022-06-07 09:09:26 +0300

    created image app

--- imageGram/models.py
+++ imageGram/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from  cloudinary.models import CloudinaryField
 
+
 class Location(models.Model):
         name = models.CharField(max_length=200)
         def __str__(self):
