18b6ba637ebc2063ac4b889eb793f259adf48ac4
b3nzuk3 <hussenito7@gmail.com>
2022-06-08 16:39:25 +0300

    deployment to heroku

--- django_app1/settings.py
+++ django_app1/settings.py
@@ -129,6 +129,7 @@ MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL= 'image-home'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
