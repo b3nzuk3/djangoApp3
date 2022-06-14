7399b5fde9960cf1d30c1285df51dde3ab596816
b3nzuk3 <hussenito7@gmail.com>
2022-06-14 08:55:29 +0300

    fixed the database

--- imageGram/models.py
+++ imageGram/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Image(models.Model):
         user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
         image = CloudinaryField('image')
-        likes = models.IntegerField(default=0)
+        website = models.CharField(max_length=100)
         comments = models.IntegerField(default=0)
         description = models.TextField(null=True, blank=True)
         created = models.DateTimeField(auto_now_add=True)
@@ -16,13 +16,16 @@ class Image(models.Model):
         #     return self.username
 
 class Comment(models.Model):
-   user = models.ForeignKey(User, on_delete=models.CASCADE)
-   post = models.ForeignKey(Image, on_delete=models.CASCADE)
-   body = models.TextField()
-   created = models.DateTimeField(auto_now_add=True)
-
-   class Meta:
+    design = models.IntegerField(null=True, default=0)
+    usability = models.IntegerField(null=True, default=0)
+    content = models.IntegerField(null=True, default=0)
+    total = models.FloatField(max_length=8, blank=True, null=True, default= 0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Image, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    class Meta:
       ordering = ['-created']
 
-   def __str__(self):
-         return self.body[0:50]
+    def __str__(self):
+        return self.body[0:50]
