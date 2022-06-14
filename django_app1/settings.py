07600e20e67ec5c6c78b2429f730089e7207260e
b3nzuk3 <hussenito7@gmail.com>
2022-06-14 14:02:19 +0300

    deploying to heroku

--- django_app1/settings.py
+++ django_app1/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
+
 
 import cloudinary
 import cloudinary.uploader
@@ -46,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
-    'corsheaders',    
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -132,7 +135,7 @@ MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL= 'image-home'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -144,3 +147,5 @@ cloudinary.config(
   api_key = "791785238298589",
   api_secret = "WPQ2gU31-wyycuPi3QHe4gPcsRY"
 )
+
+django_heroku.settings(locals())
